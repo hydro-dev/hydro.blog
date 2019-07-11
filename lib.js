8e5560a31e958eb9f8c75987d04d05508009d729
masnn <masnn0@outlook.com>
2019-07-11 22:26:16 +0800

    Markdown support

--- lib.js
+++ lib.js
@@ -1,88 +1,98 @@
 'use strict';
 
-const bson = require('bson');
+const
+    bson = require('bson'),
+    md = require('markdown-it')();
 
 module.exports = class BLOG {
     constructor(item) {
         this.db = item.db;
+        this.lib = item.lib;
     }
     get(postid) {
-        return this.db.collection('hydro_blog_posts').findOne({ id: postid });
+        return this.db.collection('posts').findOne({ id: postid });
     }
     async getMany(page) {
-        let posts = await this.db.collection('hydro_blog_posts').find()
+        let posts = await this.db.collection('posts').find()
             .sort({ time: -1 }).limit(10).skip(10 * (page - 1)).toArray();
-        let info = await this.db.collection('hydro_blog_posts').find().explain();
+        let info = await this.db.collection('posts').find().explain();
         return [posts, info.executionStats.nReturned];
     }
     getCategories() {
         return [];
     }
     async getPre(time) {
-        let res = await this.db.collection('hydro_blog_posts').find({ time: { $lt: time } })
+        let res = await this.db.collection('posts').find({ time: { $lt: time } })
             .limit(1).sort({ time: -1 }).toArray();
         if (res.length) return res[0];
         else return null;
     }
     async getNext(time) {
-        let res = await this.db.collection('hydro_blog_posts').find({ time: { $gt: time } })
+        let res = await this.db.collection('posts').find({ time: { $gt: time } })
             .limit(1).sort({ time: 1 }).toArray();
         if (res.length) return res[0];
         else return null;
     }
     async add(uid, title, type, content, tags) {
-        let user = await this.db.collection('user').findOne({ uid });
+        let user = await this.lib.user.get_by_uid(uid);
         if (!user) throw new Error('User not found!');
         let post = {
             id: new bson.ObjectID().toHexString(),
-            uid, uname: user.uname,author:user.uname, email: user.email,
+            uid, uname: user.uname, author: user.uname, email: user.email,
             title, type, content, time: new Date(), tags
         };
-        await this.db.collection('hydro_blog_posts').insertOne(post);
+        await this.db.collection('posts').insertOne(post);
         return post.id;
     }
     async addComment(id, uid, content) {
-        let post = await this.db.collection('hydro_blog_posts').findOne({ id });
+        let post = await this.db.collection('posts').findOne({ id });
         if (!post) throw new Error('Post not found!');
-        let user = await this.db.collection('user').findOne({ uid });
+        let user = await this.lib.user.get_by_uid(uid);
         if (!user) throw new Error('User not found!');
         let comment = {
             for: id, uname: user.uname, email: user.email,
             id: new bson.ObjectID().toHexString(), depth: 1,
             uid, content, time: new Date()
         };
-        return await this.db.collection('hydro_blog_comments').insertOne(comment);
+        return await this.db.collection('comments').insertOne(comment);
     }
     async replyToComment(id, uid, content) {
-        let parent = await this.db.collection('hydro_blog_comments').findOne({ id });
+        let parent = await this.db.collection('comments').findOne({ id });
         if (!parent) throw new Error('Comment not found!');
-        let user = await this.db.collection('user').findOne({ uid });
+        let user = await this.lib.user.get_by_uid(uid);
         while (parent.depth > 1)
-            parent = await this.db.collection('hydro_blog_comments').findOne({ id: parent.for });
+            parent = await this.db.collection('comments').findOne({ id: parent.for });
         if (!user) throw new Error('User not found!');
         let comment = {
             for: parent.id, uid, uname: user.uname, email: user.email,
             id: new bson.ObjectID().toHexString(), depth: 2,
             content, time: new Date()
         };
-        return await this.db.collection('hydro_blog_comments').insertOne(comment);
+        return await this.db.collection('comments').insertOne(comment);
     }
     delComment(id) {
         return Promise.all([
-            this.db.collection('hydro_blog_comments').deleteOne({ id, depth: 1 }),
-            this.db.collection('hydro_blog_comments').deleteMany({ for: id, depth: 2 })
+            this.db.collection('comments').deleteOne({ id, depth: 1 }),
+            this.db.collection('comments').deleteMany({ for: id, depth: 2 })
         ]);
     }
     getSingleComment(id) {
-        return this.db.collection('hydro_blog_comments').findOne({ id });
+        return this.db.collection('comments').findOne({ id });
     }
     async getComment(postid) {
-        let comments = await this.db.collection('hydro_blog_comments').find({ for: postid, depth: 1 }).toArray();
+        let comments = await this.db.collection('comments').find({ for: postid, depth: 1 }).toArray();
         for (let i in comments)
-            comments[i].comments = await this.db.collection('hydro_blog_comments').find({ for: comments[i].id, depth: 2 }).toArray();
+            comments[i].comments = await this.db.collection('comments').find({ for: comments[i].id, depth: 2 }).toArray();
         return comments;
     }
     getSubComment(id) {
-        return this.db.collection('hydro_blog_comments').find({ for: id, depth: 2 }).toArray();
+        return this.db.collection('comments').find({ for: id, depth: 2 }).toArray();
+    }
+    render(text, type) {
+        if (type == 'markdown') return md.render(text);
+        else if (type == 'html') return text;
+        else {
+            return 'Type not supported';
+        }
     }
 };
